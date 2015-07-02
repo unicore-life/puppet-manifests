e31d2957d19645c657c5e0873a95e880d4f04458
Rafal Kluszczynski <rkluszczynski@gmail.com>
2015-07-02 22:42:49 +0200

    modularization of plgrid manifests

--- plgrid-unicore-core/puppet-resources.pp
+++ plgrid-unicore-core/puppet-resources.pp
@@ -18,10 +18,24 @@ File {
 
 # Setting additional run stages:
 #
-stage { 'overture':
+stage { 'system':
   before => Stage['main']
+}
+
+stage { 'overture':
+  before => Stage['system']
 }
 
 stage { 'finale': }
 
 Stage['main'] -> Stage['finale']
+
+
+# Assign classes to stages:
+#
+class { 'yumrepos':
+  stage => 'system',
+}->
+class { 'package':
+  stage => 'system',
+}
